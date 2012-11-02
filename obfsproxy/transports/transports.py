04bc2bfe647dbf4c2047fe8ecde0c165a369d33c
George Kadianakis <desnacked@riseup.net>
2012-11-02 04:33:12 +0200

    Kill obfs3/dust transports. They did not work.

--- obfsproxy/transports/transports.py
+++ obfsproxy/transports/transports.py
@@ -1,16 +1,11 @@
 # XXX modulify transports and move this to a single import
 import obfsproxy.transports.dummy as dummy
 import obfsproxy.transports.b64 as b64
-# XXX broken transports
-#import obfsproxy.transports.dust_transport as dust
 import obfsproxy.transports.obfs2 as obfs2
-#import obfsproxy.transports.obfs3 as obfs3
 
 transports = { 'dummy' : {'client' : dummy.DummyClient, 'socks' : dummy.DummyClient, 'server' : dummy.DummyServer },
                'b64' : {'client' : b64.B64Client, 'socks' : b64.B64Client, 'server' : b64.B64Server },
-#               'dust' :  {'client' : dust.DustClient, 'socks' : dust.DustClient, 'server' : dust.DustServer } }
                'obfs2' : {'client' : obfs2.Obfs2Client, 'socks' : obfs2.Obfs2Client,  'server' : obfs2.Obfs2Server } }
-#               'obfs3' : {'client' : obfs3.Obfs3Client, 'socks' : obfs3.Obfs3Client,  'server' : obfs3.Obfs3Server }
 
 def get_transport_class(name, role):
     if (name in transports) and (role in transports[name]):
