9b46051cb986747f16a5266e8a90d5488e5bce1b
Alexandre Allaire <alexandre.allaire@mail.mcgill.ca>
2013-03-14 18:12:58 -0700

    Add py2exe bundling script.

---
+++ setup_py2exe.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import py2exe
+import os
+
+topdir = "py2exe_bundle"
+build_path = os.path.join(topdir, "build")
+dist_path = os.path.join(topdir, "dist")
+
+setup(
+    console=["bin/pyobfsproxy"],
+    zipfile="pyobfsproxy.zip",
+    options={
+        "build": {"build_base": build_path},
+        "py2exe": {
+            "includes": ["twisted", "pyptlib", "Crypto"],
+            "dist_dir": dist_path,
+        }
+    }
+)
